399b2c3dafb954e1a966335eeb2e17e872d33b54
Loki055 <lokeshreddy0422@gmail.com>
2024-10-02 17:44:29 -0400

    Update main.tf

--- codebase/rdicidr-0.1.0/main.tf
+++ codebase/rdicidr-0.1.0/main.tf
@@ -5,8 +5,3 @@ provider "aws" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "fsl-bucket-055"
 }
-
-resource "aws_s3_bucket_acl" "bucket_acl"{
-  bucket = aws_s3_bucket.bucket.id
-  acl = "private"
-}
